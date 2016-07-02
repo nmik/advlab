661327e7dca3f7b1c4220c5b18298b842909c59e
nmik <michela.negro@to.infn.it>
2016-07-02 14:29:48 +0000

    implemented the ch vs energy calibration

--- utils/gParsing.py
+++ utils/gParsing.py
@@ -65,23 +65,25 @@ def build_spectrum_plt(_e, **kwargs):
                  color=kwargs['color'], alpha=kwargs['alpha'])
     return h
     
-def calibrate_energy(_e, tot_en_channels, *arg):
+def calibrate_energy(_e, ref_point1, ref_point2):
     """Calibrate the measure of energy in the channels
 
        Arguments
        ---------
        _e : numpy array of floats
            Array where all the energy channels are stored for each event
-       tot_en_channels : int
+       tot_num_en_ch : int
            number of storing channels 
-       *arg : list of couples of numbers 
-           (e.g. [(ch_ref1,e_ref1),(ch_ref2,e_ref2)])
+       ref_point1, ref_point2 : each one is a couples of numbers 
+           e.g. (ch_ref1,e_ref1),(ch_ref2,e_ref2)
            It is assumed that for each couple the first num is the channel
            while the sencond is the corresponding energy [given in MeV].
     """
-    ch_array = np.arange(0., tot_en_channels)
-    return _e
-    pass
+    m = (ref_point2[1]-ref_point1[1])/(ref_point2[0]-ref_point1[0])
+    q = ref_point1[1]-ref_point1[0]*(ref_point2[1]-ref_point1[1])/\
+        (ref_point2[0]-ref_point1[0])
+    en_array = m*_e + q
+    return en_array
 
 def check_double_coinc(_t1, _t2, time_window):
     """Check events happened in coincidence, inside a coincidence window,
@@ -95,21 +97,22 @@ def check_double_coinc(_t1, _t2, time_window):
        time_window : float
            coincidence window in us
     """
-    if len(_t1[0]) < len(_t2[0]):
+    if _t1[0] > _t2[0]:
         logger.info('Excenging time arrays to maintain the sequence...')
         _ttemp =  _t1
         _t1 = _t2
         _t2 = _ttemp    
-    t2_coincident = []
-    for t_min in _t1:
+    coin_indexes = []
+    for j, t_min in enumerate(_t1):
         t_max = t_min + time_window
         _mask = (_t2 >= t_min)*(_t2 <= t_max)
         if np.count_nonzero(_mask) != 0:
-            for i,item in enumerate(_t2[_mask]):
+            for i, item in enumerate(_t2[_mask]):
                 index = np.where(_t2 == _t2[_mask][i])[0]
                 if len(index)>1:
                     index = [np.amin(index)]
-                t2_coincident.append(index)
+                coinc_indexes.append(j, index)
+    return coinc_indexes
 
 def build_coinc_curve(_t1, _t2):
     """Returns the histogram to estrapolate the coincidence curve
@@ -218,7 +221,6 @@ def process_data(file_path, num_of_channels):
     """
     logger.info('Parsing data file...')
     _ch, _t, _e = parse_data(file_path)
-    #_e = calibrate_energy(_e, TOT_NUM_EN_CH, [(200, 0.511),(300, 1.27)])
     _index = np.argsort(_t)
     _ch = _ch[_index] 
     _t = _t[_index]
@@ -226,7 +228,7 @@ def process_data(file_path, num_of_channels):
     ch, t, e = [], [], []
     logger.info('splitting the channels...')
     _t = _t*10
-    #logger.info('Time is returned in ns...')
+    logger.info('Time is returned in ns...')
     for channel in num_of_channels:
         _mask = np.where(_ch == channel)
         ch.append(_ch[_mask])
@@ -245,7 +247,6 @@ def main():
         print 'ch: %i --> time: %i, spectrum ch: %i' %(ch[0][i], t[0][i], e[0][i])
     for i in range(0,len(ch[1])):
         print 'ch: %i --> time: %i, spectrum ch: %i' %(ch[1][i], t[1][i], e[1][i])
-
 
 if __name__=='__main__':
     main()
